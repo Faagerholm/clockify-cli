bb6e5e4dbdd01e9e1b73d26d959ea74286e5f902
Jimmy Fagerholm <jimmy@identio.fi>
2020-10-09 10:01:00 +0300

    Updated readme, minor fixes to installer

--- README.md
+++ README.md
@@ -1,16 +1,21 @@
+![.github/workflows/release.yaml](https://github.com/Faagerholm/clockify-cli/workflows/.github/workflows/release.yaml/badge.svg?branch=v1.1&event=release)
+[![CII Best Practices](https://bestpractices.coreinfrastructure.org/projects/4331/badge)](https://bestpractices.coreinfrastructure.org/projects/4331)
+
 # clockify-cli
 Integrate your clocking with your favorite CLI. 
 
 ### Usage:
 ```
-  clockify-cli [flags]  
-  clockify-cli [command]
+  clockify [flags]  
+  clockify [command]
 ```
 ### Available commands:
 ```
+Available Commands:
   add-key      Add users API-KEY
   balance      Display if you're above or below zero balance.
   help         Help about any command
+  init
   off-projects Select which projects should be omitted from reports
   projects     Select default workspace project
   reset        Resets viper values
@@ -21,7 +26,9 @@ Integrate your clocking with your favorite CLI.
   workspace    Get workspaces
 
 Flags:
-      --config string   config file (default is $HOME/.clockify-cli/config.yaml) (default "./config.yaml")
+      --config string   config file (default is $HOME/.clockify-cli/config.yaml) (default "/Users/fagerholm/.clockify-cli/config.yaml")
   -h, --help            help for clockify-cli
       --viper           use Viper for configuration (default true)
+
+Use "clockify-cli [command] --help" for more information about a command.
 ```
--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ PROJECT_HOME="$HOME/.clockify-cli"
 echo 'Initializing clockify-cli'
 
 if [ -d $PROJECT_HOME ]; then
-        echo "Directory already exists"
+        echo "Project directory already exists"
 else
         $(mkdir -p $PROJECT_HOME)
         echo "$PROJECT_HOME directory is created"
@@ -21,13 +21,14 @@ fi
 echo "Creating alias for clockify"
 if [ -f "$HOME/.zshrc" ]; then
         echo "Adding it to your zshrc file."
-        # alias clockify >/dev/null 2>&1 && echo "clockify is set as an alias, skipping update of source file." || echo "alias clockify='$PROJECT_HOME/clockify-cli'" >> $HOME/.zshrc
+        alias clockify >/dev/null 2>&1 && echo "clockify is set as an alias, skipping update of source file." || echo "alias clockify='$PROJECT_HOME/clockify-cli'" >> $HOME/.zshrc
 elif [ -f "$HOME/.bash_profile" ]; then
         echo "Adding it to your bash_profile file."
         alias clockify >/dev/null 2>&1 && echo "clockify is set as an alias, skipping update of source file." || echo "alias clockify='$PROJECT_HOME/clockify-cli'" >>$HOME/.bash_profile
+else 
+        echo "Could not fine a terminal profile, please manually add 'alias clockify='$PROJECT_HOME/clockify-cli' to your profile.";
 fi
 
-os="$OSTYPE"
 processor="$(uname -m)"
 echo 'Downloading the latest release for your machine...'
 # Download the right binary, based on your OS.
@@ -162,12 +163,14 @@ else
 fi
 
 echo 'Done!'
-
+echo '-------------------'
 mv clockify-cli $PROJECT_HOME/clockify-cli
+echo 'To get started you will need a API-key. The key can be genereted on your profile page.'
 
 $PROJECT_HOME/clockify-cli init
+$PROJECT_HOME/clockify-cli user
 
-echo "Initialization completed, please run 'clockify add-key' to get started."
+echo "Initialization completed, please run 'clockify help' to get started."
 echo "You will have to restart any active terminal instance to access you newly created command."
 echo "You can also read the source file with e.g. 'source ~/.zshrc'".
 
