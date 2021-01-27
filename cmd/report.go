7c6a40a164a7ab79a5cc1a1965e5f35bff176a09
Jimmy Fagerholm <jimmy@identio.fi>
2021-01-28 00:29:06 +0200

    WIP: made report more dynamic, fixed functions

--- cmd/report.go
+++ cmd/report.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"math"
@@ -54,91 +55,161 @@ var balanceCmd = &cobra.Command{
 	Use:   "balance",
 	Short: "Display if you're above or below zero balance.",
 	Run: func(cmd *cobra.Command, args []string) {
-		key := viper.GetString("API-KEY")
-		workspace := viper.GetString("WORKSPACE")
-		user := viper.GetString("USER-ID")
-
-		if len(key) == 0 {
-			fmt.Println("API KEY NOT SET! Please run clockify app-key")
-			return
-		}
-
-		if len(user) == 0 || len(workspace) == 0 {
-			fmt.Println("User not set, please run `clockify user` to update the current user.")
-			return
-		}
 
 		loc, _ := time.LoadLocation("UTC")
 		cur_time := time.Now().In(loc)
-
-		start_time := fmt.Sprintf("%d-01-01T00:00:00Z", cur_time.Year())
 		end_time := fmt.Sprintf("%d-%02d-%02dT%02d:%02d:%02dZ", cur_time.Year(), cur_time.Month(), cur_time.Day(), cur_time.Hour(), cur_time.Minute(), cur_time.Second())
 
-		users := report_user{
-			Ids:      []string{user},
-			Contains: "CONTAINS",
-			Status:   "All",
-		}
-		filter := report_filter{
-			Groups: []string{"user", "date"},
-		}
-		reqBody, err := json.Marshal(report{
-			Start:         start_time,
-			End:           end_time,
-			SummaryFilter: &filter,
-			SortOrder:     "Ascending",
-			Users:         &users,
-		})
-		if err != nil {
-			log.Fatal(err)
-		}
-		client := &http.Client{}
-		req, _ := http.NewRequest("POST", fmt.Sprintf("https://reports.api.clockify.me/v1/workspaces/%s/reports/summary", workspace), bytes.NewBuffer(reqBody))
-		req.Header.Set("X-API-KEY", key)
-		req.Header.Set("Content-Type", "application/json; charset=utf-8")
+		var res *Result
+		res = new(Result)
 
-		resp, err := client.Do(req)
-		if err != nil {
-			log.Fatal(err)
+		for i := 0; i >= -1; i-- {
+			cur_time = cur_time.AddDate(i, 0, 0)
+			first_day := fmt.Sprintf("%d-01-01T00:00:00Z", cur_time.Year())
+			last_day := fmt.Sprintf("%d-12-31T00:00:00Z", cur_time.Year())
+			tmp_res, _ := getEntries(first_day, last_day)
+			res.addEntries(tmp_res)
 		}
 
-		defer resp.Body.Close()
+		entry := createEntry(res)
+		if entry != nil {
+			first_day := findFirstDay(entry)
+			first_day_str := fmt.Sprintf("%d-%02d-%02d", first_day.Year(), first_day.Month(), first_day.Day())
+			log.Println(first_day_str)
+			work_days := daysCalc(end_time[0:10], first_day_str)
 
-		res := new(Result)
-
-		json.NewDecoder(resp.Body).Decode(&res)
-		if res != nil {
-			// json.NewEncoder(os.Stdout).Encode(res)
-
-			// Get first day of reports (of this year)
-			first_day, _ := time.Parse("2006-01-02", res.Entries[0].Children[0].Name)
-			// today
-			today, _ := time.Parse("2006-01-02", end_time[0:10])
-			max_days := (today.Sub(first_day).Hours() / 24) + 1
-
-			// This is ugly, but works for now. Use fancy algorithm later
-			var this_day = first_day
-			for (this_day.Sub(today).Hours() / 24) <= 0 {
-				// TODO: Compare if day == holiday
-				if this_day.Weekday().String() == "Saturday" || this_day.Weekday().String() == "Sunday" {
-					max_days -= 1
-				}
-				// Add one day.
-				this_day = this_day.Add(time.Hour * 24)
+			//TODO: remove hard-coded part-time day
+			var part_time_days float64
+			part_time_percent := 1 - 0.8 // => 80%
+			if true {
+				part_time_days = daysCalc(end_time[0:10], "2020-11-01")
+			} else {
+				part_time_days = 0
 			}
-			var balance = (float64(res.Entries[0].Duration) - max_days*week_seconds) / (60 * 60)
-			fmt.Printf("----------------------\nYou have worked %.0f days.\n", max_days)
-			fmt.Printf("You have worked %.2f hours, and the recommended amount is %.2f hours.\nWhich makes your balance %dh%dmin  (%.2f)\n",
-				float64(res.Entries[0].Duration)/float64(60.0*60.0),
-				float64(max_days*week_seconds/(60*60)),
-				int(balance),
-				int((math.Abs(balance)-math.Abs(float64(int(balance))))*60),
-				balance)
 
+			log.Println(part_time_days)
+			log.Println(entry.Duration)
+			var balance = (float64(entry.Duration) - work_days*week_seconds + part_time_days*week_seconds*part_time_percent) / (60 * 60)
+			fmt.Printf("----------------------\nYou have worked %.0f days.\nYou have worked %.2f hours, and the recommended amount is %.2f hours.\nWhich makes your balance %dh%dmin  (%.2f)\nThis calculator includes today.\n",
+				work_days, // days
+				float64(entry.Duration)/float64(60.0*60.0),                                              // hours worked
+				float64((work_days*week_seconds-part_time_days*week_seconds*part_time_percent)/(60*60)), // recommended hours
+				int(balance), // balance (hours)
+				int((math.Abs(balance)-math.Abs(float64(int(balance))))*60), // balance (minutes)
+				balance) // balance (decimal)
 		} else {
-			fmt.Println("Could not get report:", err)
+			fmt.Println("Could not get report")
 		}
 	},
+}
+
+func (result *Result) addEntries(newResult *Result) []Entry {
+	result.Entries = append(result.Entries, newResult.Entries...)
+	return result.Entries
+}
+
+func findFirstDay(entry *Entry) time.Time {
+	first_day := time.Now()
+	for _, day := range entry.Children {
+		d, _ := time.Parse("2006-01-02", day.Name)
+		if first_day.Sub(d) > 0 {
+			first_day = d
+		}
+	}
+	return first_day
+}
+
+func createEntry(result *Result) *Entry {
+	var entry *Entry
+	entry = new(Entry)
+	entry.Name = result.Entries[0].Name
+	for _, e := range result.Entries {
+		entry.Children = append(entry.Children, e.Children...)
+		entry.Duration += e.Duration
+	}
+	log.Println(entry)
+	return entry
+}
+
+func getEntries(start_time string, end_time string) (*Result, error) {
+	user := viper.GetString("USER-ID")
+
+	if len(user) == 0 {
+		fmt.Println("User not set, please run `clockify user` to update the current user.")
+		return nil, errors.New("User not set, please run `clockify user`")
+	}
+
+	users := report_user{
+		Ids:      []string{user},
+		Contains: "CONTAINS",
+		Status:   "All",
+	}
+	filter := report_filter{
+		Groups: []string{"user", "date"},
+	}
+	reqBody, err := json.Marshal(report{
+		Start:         start_time,
+		End:           end_time,
+		SummaryFilter: &filter,
+		SortOrder:     "Ascending",
+		Users:         &users,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	res, err := requestReport(reqBody)
+
+	return res, err
+}
+
+func requestReport(body []byte) (*Result, error) {
+	key := viper.GetString("API-KEY")
+	workspace := viper.GetString("WORKSPACE")
+
+	if len(key) == 0 {
+		return nil, errors.New("API KEY NOT SET! Please run clockify app-key")
+	}
+
+	client := &http.Client{}
+	req, _ := http.NewRequest("POST", fmt.Sprintf("https://reports.api.clockify.me/v1/workspaces/%s/reports/summary", workspace), bytes.NewBuffer(body))
+	req.Header.Set("X-API-KEY", key)
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	res := new(Result)
+
+	json.NewDecoder(resp.Body).Decode(&res)
+	return res, err
+}
+
+func daysCalc(today string, start string) float64 {
+
+	// Get first day of reports (of this year)
+	first_day, _ := time.Parse("2006-01-02", start)
+
+	// today
+	now, _ := time.Parse("2006-01-02", today)
+	days := (now.Sub(first_day).Hours() / 24) + 1
+
+	// This is ugly, but works for now. Use fancy algorithm later
+	var this_day = first_day
+	for (this_day.Sub(now).Hours() / 24) <= 0 {
+		// TODO: Compare if day == holiday
+		if this_day.Weekday().String() == "Saturday" || this_day.Weekday().String() == "Sunday" {
+			days -= 1
+		}
+		// Add one day.
+		this_day = this_day.Add(time.Hour * 24)
+	}
+	return days
 }
 
 func stringInSlice(a string, list interface{}) bool {
